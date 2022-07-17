f791b199b2eaf6ee4abf3152e1fb92ac70a27a2b
Saffron-codes <saffrontdionysius@gmail.com>
2022-07-17 18:00:04 +0530

    added flexibility for dev & prod

--- database.py
+++ database.py
@@ -2,16 +2,21 @@ import databases
 import dotenv
 import sqlalchemy
 import os
-# DATABASE_URL = 'postgresql://postgres:1480@localhost:5432/svik_todo'
 
 username = os.environ.get("USERNAME")
 password = os.environ.get("PASSWORD")
 host = os.environ.get("HOST")
-port = os.environ.get("PORT")
 database_name = os.environ.get("DATABASE_NAME")
 
+if(password ==None):
+    username = dotenv.get_key("./.env","USERNAME")
+    password = dotenv.get_key("./.env","PASSWORD")
+    host = dotenv.get_key("./.env","HOST")
+    database_name = dotenv.get_key("./.env","DATABASE_NAME")
 
-DATABASE_URL = 'postgresql://{username}:{password}@{host}:5432/{database_name}'.format(username=username,password=password,host=host,port=port,database_name=database_name)
+
+
+DATABASE_URL = 'postgresql://{username}:{password}@{host}:5432/{database_name}'.format(username=username,password=password,host=host,database_name=database_name)
 
 
 database = databases.Database(DATABASE_URL)
--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 import os
-from dotenv import load_dotenv
+import dotenv
 
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
+host = os.environ.get("HOST")
+database_name = os.environ.get("DATABASE_NAME")
 
-print(os.environ.get("DATABASE_URL"))
+if(password ==None):
+    username = dotenv.get_key("./.env","USERNAME")
+    password = dotenv.get_key("./.env","PASSWORD")
+    host = dotenv.get_key("./.env","HOST")
+    database_name = dotenv.get_key("./.env","DATABASE_NAME")
+
+print("Username",username)
+print("Password",password)
+print("Host",host)
+print("Database Name",database_name)
+
+# [global]
+# target = /Users/Saffron/pyver/py3105/Lib/site-packages
